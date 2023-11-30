33d2b08139f4f821b3c5f2e1a13668a016279ff8
MarcosHLD <marcoshld72@gmail.com>
2023-11-30 14:04:06 -0300

    Hasta imagenes de las cards chicas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Card = ({ name, username, id }) => {
+const Card = ({ character }) => {
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
@@ -10,10 +10,10 @@ const Card = ({ name, username, id }) => {
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-        <img src=".../public/images/doctor.jpg" alt="logo de doctor" /> {/*tenemos que poner la ruta adecuada ../../ y el tañaño del logo acorde a la card*/}
-        <h4>{name}</h4>
-        <h4>{username}</h4>
-        <h4>{id}</h4>
+        <img src="../../public/images/doctor.jpg" alt="logo de doctor" style={{ maxWidth: "100%", height: "auto" }}/>
+        <h4>{character.name}</h4>
+        <h4>{character.username}</h4>
+        <h4>{character.id}</h4>
 
       
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
