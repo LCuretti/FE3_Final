9685e8aac65af0d1bed985ab4d499743dcbbcd59
MarcosHLD <marcoshld72@gmail.com>
2023-11-30 11:58:36 -0300

    Rutas ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 
+import { Route,Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
+import Contact from "./Routes/Contact";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Routes>
-            
-          </Routes>
-          <Footer/>
-      </div>
+      <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/favs" element={<Favs/>}/>
+          <Route path="/detail" element={<Detail/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+        <Footer/>
+      </>
   );
 }
 
