74de548a247e14fafb9bf7f732bcef491965d5be
LCuretti <curetti.luis@gmail.com>
2023-12-02 00:12:42 -0300

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,17 @@
-import "../index.css";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer>
       <p>Powered by</p>
-      <img src="/images/DH.png" alt="DH-logo" />
-      <img src="/images/ico-facebook.png" alt="Icono de Facebook" />
-      <img src="/images/ico-instagram.png" alt="Icono de Instagram" />
-      <img src="/images/ico-tiktok.png" alt="Icono de Tiktok" />
-      <img src="/images/ico-whatsapp.png" alt="Icono de Whatsapp" />
+      <img style={{width: '200px', margin: '0'}} src="/images/DH.png" alt="DH-logo" />
+
+      <>
+        <img src="/images/ico-facebook.png" alt="Icono de Facebook" />
+        <img src="/images/ico-instagram.png" alt="Icono de Instagram" />
+        <img src="/images/ico-tiktok.png" alt="Icono de Tiktok" />
+        <img src="/images/ico-whatsapp.png" alt="Icono de Whatsapp" />
+      </>
     </footer>
   );
 };
